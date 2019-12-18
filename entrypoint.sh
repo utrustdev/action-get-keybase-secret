c927e1906bb91ca8f508c7629214062e8ca725e6
Miguel Palhas <mpalhas@gmail.com>
2019-12-18 14:49:04 +0000

    code review

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ git clone $repo $HOME/secrets
 
 ls $HOME/secrets
 echo $output
-cat $HOME/secrets/file
+cat $HOME/secrets/$file
 cp $HOME/secrets/$file $output
 
 echo ::set-output name=file::$output
