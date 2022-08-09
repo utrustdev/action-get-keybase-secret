3363a804823bd82fff470eb242e980af66966dd5
Bennie Mosher <benniemosher@gmail.com>
2022-08-09 14:08:19 -0600

    PE-463: Protected with quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ue
 
-repo=$1
-file=$2
-prefix=$3
-output=${prefix}${file/\//-}
+repo="${1}"
+file="${2}"
+prefix="${3:-keybase-secret}"
+output="${prefix}${file/\//-}"
 
 export KEYBASE_ALLOW_ROOT=1
 
@@ -13,6 +13,6 @@ keybase oneshot
 
 git clone $repo $HOME/secrets
 
-cp $HOME/secrets/$file $output
+cp "${HOME}/secrets/${file}" "${output}"
 
 echo ::set-output name=file::$output
