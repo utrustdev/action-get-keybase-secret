b02f809e4020b392ce9ea804354c3e066b2d9563
brice burgess <brice.burgess@fundthatflip.com>
2022-08-10 12:34:03 -0600

    PE-463: shellcheck fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-repo="${1}"
-file="${2}"
-prefix="${3:keybase-secret-}"
+repo="$1"
+file="$2"
+prefix="${3-keybase-secret-}"
 output="${prefix}${file/\//-}"
 
 export KEYBASE_ALLOW_ROOT=1
@@ -12,8 +12,8 @@ run_keybase
 sleep 5
 keybase oneshot
 
-git clone $repo $HOME/secrets
+git clone "$repo" "$HOME/secrets"
 
-cp "${HOME}/secrets/${file}" "${output}"
+cp "$HOME/secrets/$file" "${output}"
 
-echo ::set-output name=file::$output
+echo "::set-output name=file::$output"
